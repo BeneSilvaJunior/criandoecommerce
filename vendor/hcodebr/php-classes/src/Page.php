4e0196b2bba6aeae1d58d51f02223ae00ed371ce
Bene Silva Junior <benesilvajunior@gmail.com>
2020-09-03 21:29:16 -0300

    tpl site

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,37 +1,54 @@
 <?php
 
-namespace Hcode;
+namespace Hcode; //namespace defina a localização da classe Page, neste caso em Hcode
 
-use Rain\Tpl;
+use Rain\Tpl; //usa outro namespace, o Rain\Tpl (ao chamar new Tpl() virá do namespace Rain\Tpl)
 
+//Classe Page para gerenciar as telas, o HTML
 class Page {
 	
 	private $tpl;
 	private $options = [];
+	
+	//Passamos as variáveis para o template (é um array vazio por padrão)
 	private $defaults = [
 		"data"=>[]
 	];
 
- public function __construct($opts = array()) {
+ //Método mágico - primeiro a ser carregado
+ 
+ /* Passamos as opções "$opts = array" (por padrão é um array). 
+ Mas podemos ter algumas opções padrão (default) "$defaults" (é um array) */
+ public function __construct($opts = array()) {	 
 	 
 	 $this->options = array_merge($this->defaults, $opts);
  
-    // config
+    // configuração do template
 	$config = array(
-		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-		"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/", //pasta para pegar os templates
+		"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/", //pasta com o cache HTML
 		"debug"         => false // set to false to improve the speed
+		
+		//$_SERVER["DOCUMENT_ROOT"] define a pasta root do servidor
 	);
 
+    //Passa as configurações para o Tpl (use configurado "Rain\Tpl")
 	Tpl::configure( $config );
 	
+	/* Para termos acesso ao tpl em outros métodos, é necessário que ele seja um atributo desta classe. 
+	Para isso usamos a palavra-chave $this >>  private $tpl */
 	$this->tpl = new Tpl;	
 	
+	/* Passamos as variáveis, que vem de acordo com a rota, que depende da rota que vc estiver chamando do slim. 
+	Assim passamos os dados para a classe Page. Para isso recebemos algumas opções da classe */
 	$this->setData($this->options["data"]);
 	
 	$this->tpl->draw("header");
  
  }
+ 
+ //Vamos juntar as opções que forem passadas com os default (fazer um merge)
+ 
  
  private function setData($data = array())
  {
@@ -47,6 +64,7 @@ class Page {
 	 return $this->tpl->draw($name, $returnHTML);
  }
  
+ //Metodo destrutor - último a ser carregado
  public function __destruct() {
 	 
  $this->tpl->draw("footer");
